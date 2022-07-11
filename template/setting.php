54fc2591677fe1bb59f5c2ba4147153621774eb8
daniindosoft <dani@indosoft.dk>
2022-07-11 16:46:09 +0700

    update panduan

---
+++ template/setting.php
@@ -0,0 +1,15 @@
+<h4>Penjelasan Menu : Settings</h4>
+<p>
+	Halaman ini memiliki 2 fungsi yaitu menambahkan Style Tombol dan menambahkan nomor whatsapp
+</p>
+<hr>
+<div style="padding: 10px;background: #f0f0f0;border-radius: 4px;">	
+	<h5>Hubungkan</h5>
+	<p>Tombol atau form akan mengarah ke nomor WhatsApp ini</p>
+	<hr>
+	<h5>Style Tombol</h5>
+	<p>
+		Tab ini Untuk menambahkan Style tombol yang bisa Anda pilih di 'Tema Tombol' di menu Template
+	</p>
+	<hr>
+</div>
---
+++ template/template.php
@@ -0,0 +1,41 @@
+<h4>Penjelasan Menu : Template</h4>
+<p>Halaman ini berfungsi untuk membuat template atau format text saat Pembeli/pengunjung mengklik tombol whatsapp, berikut penjelasan setiap fieldnya :</p>
+<hr>
+<div style="padding: 10px;background: #f0f0f0;border-radius: 4px;">	
+	<h5>Judul Template</h5>
+	<p>Field ini untuk memberi nama/judul template yang akan kamu buat, <u>nama template ini tidak akan terlihat oleh pembeli</u> </p>
+	<hr>
+	<h5>Event</h5>
+	<p>
+		Di field ini terdapat 2 pilihan :
+		<ul style="list-style: disc;margin-left: 19px;">
+			<li><b>Standard :</b> jika ini dipilih maka pembeli ketika klik tombol akan langsung diarahkan ke whatsapp</li>
+			<li><b>Form :</b> Ini digunakan jika Anda ingin membuat tombol dengan memunculkan form yang bisa di isi oleh pembeli terlebih dahulu sebelum meredirek ke whatsapp</li>
+		</ul>
+	</p>
+	<hr>
+	<h5>Template Text</h5>
+	<p>Untuk membuat konten atau isi pesan, Anda bisa memasukan disini (Khusus untuk Event Standard)</p>
+	<hr>
+	<h5>Pilih Form</h5>
+	<p>Anda bisa memilih jenis form yang bisa anda gunakan ketika pembeli klik tombol (Khusus untuk event Form)</p>
+	<hr>
+	<h5>Text di Tombol</h5>
+	<p>
+		Untuk merubah atau memasukan text pada tombol
+	</p>
+	<hr>
+	<h5>Tema Tombol</h5>
+	<p>
+		Tema tombol atau style tombol jika Anda ingin mengcustomnya
+	</p>
+	<hr>
+	<h5>Penerapan</h5>
+	<p>Tombol atau template akan di terapkan di produk yang telah Anda pilih di bagian ini</p>
+	<hr>
+	<h5>Pengaturan</h5>
+	<p>
+		Disini Anda bisa menseting posisi tombol akan di tampilkan
+	</p>
+	<hr>
+</div>
